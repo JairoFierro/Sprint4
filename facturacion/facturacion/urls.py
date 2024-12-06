a3ca867158ddc4073ab8fe9ce5eebfe1732a23f3
Marcosespa <marcosespa24@gmail.com>
2024-12-06 07:32:22 -0500

    Cambios

--- facturacion/facturacion/urls.py
+++ facturacion/facturacion/urls.py
@@ -19,5 +19,5 @@ from django.urls import include, path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('facturacion.urls')),
+    path('', include('app_facturacion.urls')),
 ]
