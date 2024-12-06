2a6810fcea50aaecec76e291d3331f94dc2c0731
Jairo <ja.fierro@uniandes.edu.co>
2024-12-06 11:05:10 -0500

    Cambio2

--- institucion/institucion/settings.py
+++ institucion/institucion/settings.py
@@ -129,4 +129,4 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 PATH_API_GATEWAY = "http://" + os.environ.get("KONG_HOST", "10.128.0.81") + ":" + os.environ.get("KONG_PORT", "8000")
-PATH_VAR = PATH_API_GATEWAY + "/instituciones"
+PATH_VAR = PATH_API_GATEWAY + "/"
