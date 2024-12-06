69e3ff350b5f5b590006482655e1d8aa92f1d719
Jairo <ja.fierro@uniandes.edu.co>
2024-12-06 14:42:48 -0500

    Reporte

--- reporte/app_reporte/views.py
+++ reporte/app_reporte/views.py
@@ -2,17 +2,17 @@ from django.shortcuts import render
 from facturacion.app_facturacion.models import Factura
 
 def vista_reporte_facturas(request):
-    facturas = Factura.objects.all()
-    reporte = [
-        {
-            'id': factura.id,
-            'estudiante': factura.estudiante.nombre,
-            'estado': factura.estado_actual(), 
-            'saldo_pendiente': factura.calcular_saldo_pendiente(),  
-            'monto_total': factura.monto_total,
-            'fecha_emision': factura.fecha_emision,
-        }
-        for factura in facturas
-    ]
-    return render(request, 'reporte_facturas.html', {'reporte': reporte})
-
+    # facturas = Factura.objects.all()
+    # reporte = [
+    #     {
+    #         'id': factura.id,
+    #         'estudiante': factura.estudiante.nombre,
+    #         'estado': factura.estado_actual(), 
+    #         'saldo_pendiente': factura.calcular_saldo_pendiente(),  
+    #         'monto_total': factura.monto_total,
+    #         'fecha_emision': factura.fecha_emision,
+    #     }
+    #     for factura in facturas
+    # ]
+    # return render(request, 'reporte_facturas.html', {'reporte': reporte})
+    return render(request, 'reporte_facturas.html')
