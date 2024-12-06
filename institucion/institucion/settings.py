521d2913fbf3d4a5cd439c9fa29983fdedb4179c
Marcosespa <marcosespa24@gmail.com>
2024-12-06 10:09:09 -0500

    dede

--- institucion/institucion/settings.py
+++ institucion/institucion/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'institucion'
+    'app_institucion',
 ]
 
 MIDDLEWARE = [
