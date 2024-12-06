8a909176642641896d81b8daf25dfd34f00d804a
Valentina Ceron <l.ceronp@uniandes.edu.co>
2024-12-06 10:37:29 -0500

    db reporte

--- reporte/reporte/settings.py
+++ reporte/reporte/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'reporte.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': os.environ.get("REPORTE_DB", "reporte_db"),
+        'USER': os.environ.get("REPORTE_DB_USER", "reporte_user"),
+        'PASSWORD': os.environ.get("REPORTE_DB_PASSWD", "isis2503"),
+        'HOST': os.environ.get("REPORTE_DB_HOST", "10.128.0.83"),
+        'PORT': os.environ.get("REPORTE_DB_PORT", "5432")
     }
 }
 
