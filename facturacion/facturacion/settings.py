5e8f8ff453dc63354dc5b0d50d2fd59f166ba8ec
Marcosespa <marcosespa24@gmail.com>
2024-12-06 10:45:19 -0500

    de

--- facturacion/facturacion/settings.py
+++ facturacion/facturacion/settings.py
@@ -39,7 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'app_facturacion',
-    'institucion',
+    'institucion.app_institucion',
     ]
 
 MIDDLEWARE = [
